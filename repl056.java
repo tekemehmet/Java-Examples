daa24348e9cfb2148b92b158de391834d082ebd0
tekemehmet <tekemehmet@yahoo.com>
2019-12-10 17:29:15 -0500

    Add files via upload

---
+++ repl056.java
@@ -0,0 +1,29 @@
+package replyStudy;
+
+import java.util.Scanner;
+
+public class repl056 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter command:");
+        char response = scan.next().charAt(0);
+
+        switch (response) {
+            case 'y':
+                System.out.println("Your request is being processed");
+                break;
+            case 'n':
+                System.out.println("Thank you anyway for your consideration");
+                break;
+            case 'h':
+                System.out.println("Sorry, no help is currently available");
+                break;
+            default:
+                System.out.println("Invalid entry, please try again!");
+
+        }
+
+    }
+}
