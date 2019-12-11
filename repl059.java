5fce79c15ba90a8e57d25b478b2ad552e3e04e0c
tekemehmet <tekemehmet@yahoo.com>
2019-12-11 17:16:19 -0500

    Add files via upload

---
+++ repl059.java
@@ -0,0 +1,27 @@
+package replyStudy;
+
+import java.util.Scanner;
+
+public class repl059 {
+
+    public static void main(String[] args) {
+
+        // Assume that you have only 2 users: Max Payne and Alan Wake.
+        // First, ask user to enter full name. Display message: "Enter full name:".
+        // Then take input from user. If name is equals to either "Max Payne" or "Alan Wake",
+        // display message: "User found!". Otherwise,  display message: "User not found!".
+        // Please make your search case insensitive!
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter full name:");
+        String name = scan.nextLine();
+
+        if( name.equalsIgnoreCase("Max Payne") || name.equalsIgnoreCase("Alan Wake")  ){
+            System.out.println("User found!");
+        } else {
+            System.out.println("User not found!");
+        }
+
+
+    }
+}
