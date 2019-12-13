18a62e56cebd3b956fa48613582e38807bbb23ff
tekemehmet <tekemehmet@yahoo.com>
2019-12-13 17:01:31 -0500

    Add files via upload

---
+++ repl063.java
@@ -0,0 +1,14 @@
+package replyStudy;
+
+import java.util.Scanner;
+
+public class repl063 {
+    public static void main(String[] args) {
+
+        //Write a program that will print out first character of the word
+        Scanner scan = new Scanner(System.in);
+        String word = scan.next();
+
+        System.out.println(word.charAt(0));
+    }
+}
