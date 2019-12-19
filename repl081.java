7dfe79d340a744dde89be8c37c381eeb5d83cf3d
tekemehmet <tekemehmet@yahoo.com>
2019-12-19 16:23:59 -0500

    Add files via upload

---
+++ repl081.java
@@ -0,0 +1,13 @@
+package replyStudy;
+
+public class repl081 {
+
+    public static void main(String[] args) {
+        int x = 0;
+        while(  x <= 100) {
+            System.out.println(x);
+            x++;
+        }
+
+    }
+}
