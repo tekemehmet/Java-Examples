905b39edf8df76b37c64d8a75f78a6c9a4be8ad8
tekemehmet <tekemehmet@yahoo.com>
2019-12-20 21:11:54 -0500

    Add files via upload

---
+++ repl085.java
@@ -0,0 +1,18 @@
+package replyStudy;
+
+public class repl085 {
+    public static void main(String[] args) {
+
+        // Write for and while loops so that they print those numbers between 1 and 20 and divisible by 3.
+        //Expected Output:  3 6 9 12 15 18
+
+        for (int i = 1; i <21 ; i++) {
+            if(i % 3 == 0){
+                System.out.print(i + " ");
+            }
+
+        }
+
+
+    }
+}
