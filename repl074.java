1f83df324f737dcc189170cef11e272d433d3903
tekemehmet <tekemehmet@yahoo.com>
2019-12-25 08:04:37 -0500

    Add files via upload

---
+++ repl074.java
@@ -0,0 +1,23 @@
+package replyStudy;
+
+import java.util.Scanner;
+
+public class repl074 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+
+        if(a.contains("alejandro")){
+            System.out.println("read this mail");
+        } else {
+            System.out.println("don't read");
+        }
+
+
+
+    }
+
+
+}
