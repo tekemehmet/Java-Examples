a8276b39e80d0661ed319e12765668844b0e56ca
tekemehmet <tekemehmet@yahoo.com>
2019-12-26 09:33:01 -0500

    Add files via upload

---
+++ repl052.java
@@ -0,0 +1,33 @@
+package replyStudy;
+
+import java.util.Scanner;
+
+public class repl052 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("what is the farthest planet in the solar system:" );
+        System.out.println("a)venus" );
+        System.out.println("b)pluto" );
+        System.out.println("c)neptune" );
+
+        String a = s.nextLine();
+
+        //your code here
+        if (a.equals("a")) {
+            System.out.println(a + " is wrong");
+        } else if ( a.equals("b")) {
+            System.out.println(a + " is correct");
+        } else if (a.equals("c")) {
+            System.out.println(a + " is wrong");
+        } else {
+            System.out.println(a + " is not a valid answer");
+        }
+
+
+
+
+    }
+}
