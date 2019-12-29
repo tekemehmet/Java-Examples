748dfe2b607a09ece1e28fc0e3a6891e12f6d776
tekemehmet <tekemehmet@yahoo.com>
2019-12-29 10:56:52 -0500

    Add files via upload

---
+++ repl112.java
@@ -0,0 +1,17 @@
+package replyStudy;
+
+public class repl112 {
+    public static void main(String[] args) {
+
+        // Given an int variable k that has already been declared,
+        // use a for loop to print a single line consisting of 97 asterisks.
+        // Use no variables other than k.
+
+        int k = 1;
+        for ( k = 0; k < 97; k++) {
+            System.out.print("*");
+
+        }
+
+    }
+}
