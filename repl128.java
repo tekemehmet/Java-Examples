8a815487057065f54eeb7c15841560828b668413
tekemehmet <tekemehmet@yahoo.com>
2020-01-02 09:38:05 -0500

    Add files via upload

---
+++ repl128.java
@@ -0,0 +1,34 @@
+package replyStudy;
+
+import java.util.Scanner;
+
+public class repl128 {
+
+    public static void main(String[] args) {
+
+        // Given an array temps of doubles, containing temperature data, compute the average temperature.
+        //Store the average in a variable called avgTemp.
+        //Besides temps and avgTemp, you may use only two other variables -- an int variable k and a double variable named total, which have been declared.
+        //Example:
+        //input: 80 88 88 84 82 78 60 68
+        //output: 78.5
+
+        Scanner scan = new Scanner(System.in);
+        int k = 0;
+        double total = 0;
+        double avgTemp = 0;
+        double[] temps = { scan.nextDouble(), scan.nextDouble(), scan.nextDouble(), scan.nextDouble(),
+                scan.nextDouble(), scan.nextDouble(), scan.nextDouble() , scan.nextDouble()  };
+
+        for (int i = 0; i < temps.length ; i++) {
+
+            avgTemp += temps[i]/ (temps.length);
+
+        }
+        System.out.println(avgTemp);
+
+
+
+
+    }
+}
