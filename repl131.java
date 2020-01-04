29bb162b7b953bc45400cc059e76c1b80daea3f5
tekemehmet <tekemehmet@yahoo.com>
2020-01-03 23:18:06 -0500

    Add files via upload

---
+++ repl131.java
@@ -0,0 +1,19 @@
+package replyStudy;
+
+public class repl131 {
+
+    public static void main(String[] args) {
+
+        // Write a for loop that prints all the even integers from 80 through 20 inclusive, separated by spaces.
+
+
+        for (int i = 80; i >=20 ; i--) {
+
+            if( i % 2 == 0){
+                System.out.print(i+" ");
+            }
+
+        }
+
+    }
+}
