5fe11e01db4b26292cc3ea024832e3e9f36ecd1a
tekemehmet <tekemehmet@yahoo.com>
2020-01-07 18:51:11 -0500

    Add files via upload

---
+++ repl149.java
@@ -0,0 +1,22 @@
+package replyStudy;
+
+public class repl149 {
+
+
+    public static void main(String[] args) {
+
+        // Given 2 static methods hello() and world().
+        // Provide an implementation for both of them in order to print appropriate message.
+        // So hello() method should print "Hello" and world() method should print "World!".
+        //output: Hello World!
+        hello();
+        world();
+
+    }
+    public static void hello(){
+        System.out.print("Hello ");
+    }
+    public static void world(){
+        System.out.println("World!");
+    }
+}
