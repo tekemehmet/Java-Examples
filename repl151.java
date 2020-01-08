a5b6fc12f934dc7fb2bb12195505c2c983e006e0
tekemehmet <tekemehmet@yahoo.com>
2020-01-08 17:59:59 -0500

    Add files via upload

---
+++ repl151.java
@@ -0,0 +1,41 @@
+package replyStudy;
+
+import java.util.Scanner;
+
+public class repl151 {
+
+    // a sign function is simple, its gets a number and tells you if its positive, negative or zero.
+    //
+    //for example :
+    //sign(5)  => 1
+    //sign(-30)=> -1
+    //sign(0)  => 0
+    //
+    //sign gets an int parameter.
+    //
+    //print out 1,-1 or 0 depending on the input
+
+
+    public static void sign(int n){
+
+        if ( n > 0) {
+            System.out.println("1");
+        } else if ( n < 0) {
+            System.out.println("-1");
+        } else {
+            System.out.println(0);
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+
+        sign(n);
+    }
+}
